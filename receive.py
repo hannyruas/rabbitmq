856144f501a397fc3e1101877986e140f05b8921
hannyruas <hannyruas@gmail.com>
2018-10-28 01:28:16 +0200

    Final step 3

--- receive.py
+++ receive.py
@@ -16,7 +16,7 @@ channel.queue_bind(exchange='logs',
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 
-def callback(body):
+def callback(ch, method, properties, body):
     body_str = "".join(map(chr, body))
     message_info = body_str.split("  ")
     data_path = message_info[0]
